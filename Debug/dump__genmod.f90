3c4d20599445bb4d5e035a1ad25744bfa4f9681c
shangjiaxuan <34389517+shangjiaxuan@users.noreply.github.com>
2018-01-24 20:41:34 +0800

    Fix

---
+++ Debug/dump__genmod.f90
@@ -0,0 +1,11 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Wed Jan 24 20:39:45 2018
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE DUMP__genmod
+          INTERFACE 
+            SUBROUTINE DUMP(NSTP,E)
+              INTEGER(KIND=4) :: NSTP
+              REAL(KIND=4) :: E
+            END SUBROUTINE DUMP
+          END INTERFACE 
+        END MODULE DUMP__genmod
